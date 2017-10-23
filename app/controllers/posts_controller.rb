ddb0da3325267753c32900972926b5306576548a
mau5atron <gabrielbetancourt96@outlook.com>
2017-10-23 09:35:03 -0400

    Comitting, edit, show, destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	# before_action :set_post!, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@posts = Post.all
 	end
@@ -12,6 +14,29 @@ class PostsController < ApplicationController
 		redirect_to posts_path
 	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+		# refers to specific post we are looking at
+	end
+
+	def update
+		# This action will be updating things in our database
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to(posts_path(@post))
+
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+
+	end
 
 	private 
 	def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   root 'posts#index'
+  get 'posts/new' => 'posts#new'
+  post 'posts' => 'posts#create'
+  get 'posts/:id' => 'posts#show'
+  get 'posts/:id/edit' => 'posts#edit'
+  put 'posts/:id' => 'posts#update'
+  delete 'posts/:id' => 'posts#destroy'
 end
